74caf876afef6213ff17917658e2a565c5edc700
MajedZarkash1 <majedzarkash@gmail.com>
2024-09-24 15:16:16 +1000

    Platform game
    I have added a skin for the player and walking animation

--- platformer game/platformer-game/scenes/main_character.gd
+++ platformer game/platformer-game/scenes/main_character.gd
@@ -7,6 +7,10 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
+	if (velocity.x > 1 || velocity.x < -1):
+		sprite_2d.animation = "walking animation"
+	else:
+		sprite_2d.animation = "default"
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
