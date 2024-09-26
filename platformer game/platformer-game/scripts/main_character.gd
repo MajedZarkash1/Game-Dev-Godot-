0a2d68eb326d6781ac54c829261510401fc51d2e
MajedZarkash1 <majedzarkash@gmail.com>
2024-09-26 15:33:06 +1000

    .

--- platformer game/platformer-game/scripts/main_character.gd
+++ platformer game/platformer-game/scripts/main_character.gd
@@ -1,19 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 260.0
 const JUMP_VELOCITY = -400.0
 @onready var sprite_2d: AnimatedSprite2D = $Sprite2D
-
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta: float) -> void:
 	if (velocity.x > 1 || velocity.x < -1):
 		sprite_2d.animation = "walking animation"
 	else:
 		sprite_2d.animation = "default"
-	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
+		sprite_2d.animation = "jumping animation"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -25,9 +25,10 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 20)
 
 	move_and_slide()
+	
 	
 	
 	var isLeft = velocity.x < 0
