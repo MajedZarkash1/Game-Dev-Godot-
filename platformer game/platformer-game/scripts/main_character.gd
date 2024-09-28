1d41e111419645850a7b2a17c466b97d554f1c47
MajedZarkash1 <majedzarkash@gmail.com>
2024-09-28 12:20:46 +1000

    .

--- platformer game/platformer-game/scripts/main_character.gd
+++ platformer game/platformer-game/scripts/main_character.gd
@@ -2,10 +2,44 @@ extends CharacterBody2D
 
 
 const SPEED = 260.0
-const JUMP_VELOCITY = -450.0
+const JUMP_VELOCITY = -400.0
 @onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var health_bar: ProgressBar = $HealthBar
 
+
+
+#       Health
+var max_health : int =100
+var current_health: int = max_health
+#       Health
+
+
+
+func _ready():
+	health_bar.max_value = max_health
+	health_bar.value = current_health
+	
+	
+func take_damage(amount: int):
+	current_health -= amount
+	current_health = clamp(current_health, 0, max_health)
+	
+	#update health bar
+	health_bar.value = current_health
+	
+	
+	if current_health <= 0:
+		die()
+		
+		
+func die():
+		# I can add animation for dying here
+		
+	queue_free()
+	
+	
+	
 func _physics_process(delta: float) -> void:
 	if (velocity.x > 1 || velocity.x < -1):
 		sprite_2d.animation = "walking animation"
