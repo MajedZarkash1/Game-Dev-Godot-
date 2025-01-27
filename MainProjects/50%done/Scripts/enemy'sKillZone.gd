d9b8e29425e42fba48b721555e4a2b2088d07c1d
MajedZarkash1 <majedzarkash@gmail.com>
2025-01-27 13:58:15 +1100

    .

---
+++ MainProjects/50%done/Scripts/enemy'sKillZone.gd
@@ -0,0 +1,18 @@
+extends Area2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+
+@onready var timer: Timer = $Timer
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print("youDied!")
+	timer.start()
+	
+	
+
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
+	
--- platformer game/platformer-game/scripts/global.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-var Player_current_attack = false
