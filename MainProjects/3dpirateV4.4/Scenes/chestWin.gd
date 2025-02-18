38e0de0055889612acf669643fc90620d1603f9e
MajedZarkash1 <majedzarkash@gmail.com>
2025-02-18 15:41:56 +1100

    .

--- MainProjects/3dpirateV4.4/Scenes/chestWin.gd
+++ MainProjects/3dpirateV4.4/Scenes/chestWin.gd
@@ -3,5 +3,5 @@ extends Area3D
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
-		body.get_tree().reload_current_scene()
+		body.get_tree().change_scene_to_file("res://Scenes/map_2.tscn")
 	pass # Replace with function body.
